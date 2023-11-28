5603b3cb4f8628fc57f485f6cb5f0f80051f53f3
bburciaga <burciagab16@gmail.com>
2023-11-28 13:34:25 -0800

    added two more command classes

---
+++ app/src/main/java/com/example/arhms/utils/command/HeartRateLow.java
@@ -0,0 +1,10 @@
+package com.example.arhms.utils.command;
+
+import com.example.arhms.interfaces.IAlertCommand;
+
+public class HeartRateLow implements IAlertCommand {
+    @Override
+    public void execute() {
+
+    }
+}

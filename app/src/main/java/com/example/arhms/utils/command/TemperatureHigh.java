1597c16f06731b6b5312449d8381b76ff1574309
bburciaga <burciagab16@gmail.com>
2023-11-28 13:34:25 -0800

    added two more command classes

---
+++ app/src/main/java/com/example/arhms/utils/command/TemperatureHigh.java
@@ -0,0 +1,10 @@
+package com.example.arhms.utils.command;
+
+import com.example.arhms.interfaces.IAlertCommand;
+
+public class TemperatureHigh implements IAlertCommand {
+    @Override
+    public void execute() {
+
+    }
+}

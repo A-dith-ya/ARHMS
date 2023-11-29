87ef9d370e3b683ae49e612c6628edda754ff48f
bburciaga <burciagab16@gmail.com>
2023-11-28 23:57:25 -0800

    fixed compilation errors and updated Concrete Command classes

--- app/src/main/java/com/example/arhms/utils/command/HeartRateLow.java
+++ app/src/main/java/com/example/arhms/utils/command/HeartRateLow.java
@@ -3,6 +3,12 @@ package com.example.arhms.utils.command;
 import com.example.arhms.interfaces.IAlertCommand;
 
 public class HeartRateLow implements IAlertCommand {
+    private AlertSystem alertSystem;
+
+    public HeartRateLow (AlertSystem alertSystem) {
+        this.alertSystem = alertSystem;
+    }
+
     @Override
     public void execute() {
 

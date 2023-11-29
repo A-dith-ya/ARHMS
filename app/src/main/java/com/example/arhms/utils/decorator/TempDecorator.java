21e12318a8f6df3be4f12599c7a2ff8a3d61095c
A-dith-ya <awedithya@gmail.com>
2023-11-29 00:13:15 -0800

    Add temp image and display changes to TempDecorator

--- app/src/main/java/com/example/arhms/utils/decorator/TempDecorator.java
+++ app/src/main/java/com/example/arhms/utils/decorator/TempDecorator.java
@@ -1,8 +1,12 @@
 package com.example.arhms.utils.decorator;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.arhms.R;
 import com.example.arhms.abstracts.UserMetricDecorator;
 import com.example.arhms.interfaces.UserCustomizablePage;
 
@@ -17,8 +21,21 @@ public class TempDecorator extends UserMetricDecorator {
 
     @Override
     public void addMetric() {
+        LinearLayout linearLayout = new LinearLayout(getContext());
+
+        ImageView tempImageView = new ImageView(getContext());
+        tempImageView.setImageResource(R.drawable.temp_icon);
+        tempImageView.setPadding(10, 10, 5, 10);
+
         TextView tempMetricTextView = new TextView(getContext());
-        tempMetricTextView.setText("Temp Metric: Your temp value");
-        decoratedPage.addView(tempMetricTextView);
+        tempMetricTextView.setText("Temp: 120 C");
+        tempMetricTextView.setTextColor(Color.RED);
+        tempMetricTextView.setTextSize(18);
+        tempMetricTextView.setPadding(0, 10, 0, 10);
+
+        linearLayout.addView(tempImageView);
+        linearLayout.addView(tempMetricTextView);
+
+        decoratedPage.addView(linearLayout);
     }
 }

7fb069494b858f303bf144589b3432dbfc8ad3c3
A-dith-ya <awedithya@gmail.com>
2023-11-29 15:57:28 -0800

    Add dashboard activity showing overview of user features

---
+++ app/src/main/java/com/example/arhms/fragments/DashboardActivity.java
@@ -0,0 +1,15 @@
+package com.example.arhms.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.arhms.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}

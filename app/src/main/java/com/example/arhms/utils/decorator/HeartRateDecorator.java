a96ad97bfb855d8ce3a3101fdcb4e995f90a4413
A-dith-ya <awedithya@gmail.com>
2023-11-28 23:45:09 -0800

    Add heart image and display changes to HeartRateDecorator

--- app/src/main/java/com/example/arhms/utils/decorator/HeartRateDecorator.java
+++ app/src/main/java/com/example/arhms/utils/decorator/HeartRateDecorator.java
@@ -1,11 +1,14 @@
 package com.example.arhms.utils.decorator;
 
 import android.content.Context;
-import android.view.View;
+import android.graphics.Color;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.arhms.abstracts.UserMetricDecorator;
 import com.example.arhms.interfaces.UserCustomizablePage;
+import com.example.arhms.R;
 
 public class HeartRateDecorator extends UserMetricDecorator {
     private UserCustomizablePage decoratedPage;
@@ -18,8 +21,21 @@ public class HeartRateDecorator extends UserMetricDecorator {
 
     @Override
     public void addMetric() {
+        LinearLayout linearLayout = new LinearLayout(getContext());
+
+        ImageView heartImageView = new ImageView(getContext());
+        heartImageView.setImageResource(R.drawable.heart_icon);
+        heartImageView.setPadding(10, 10, 5, 10);
+
         TextView heartRateMetricTextView = new TextView(getContext());
-        heartRateMetricTextView.setText("Heart Metric: Your heart rate value");
-        decoratedPage.addView(heartRateMetricTextView);
+        heartRateMetricTextView.setText("Abnormal Heart Rate: 120 bpm");
+        heartRateMetricTextView.setTextColor(Color.RED);
+        heartRateMetricTextView.setTextSize(18);
+        heartRateMetricTextView.setPadding(0, 10, 0, 10);
+
+        linearLayout.addView(heartImageView);
+        linearLayout.addView(heartRateMetricTextView);
+
+        decoratedPage.addView(linearLayout);
     }
 }

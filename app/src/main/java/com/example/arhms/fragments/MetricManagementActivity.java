e78325150cdef34cc1f944e67b1bb221f505dbf3
A-dith-ya <awedithya@gmail.com>
2023-11-29 17:39:54 -0800

    Add MetricManagementActivity for choosing metric functionality to be added to CustomizableFragment

---
+++ app/src/main/java/com/example/arhms/fragments/MetricManagementActivity.java
@@ -0,0 +1,15 @@
+package com.example.arhms.fragments;
+
+import com.example.arhms.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MetricManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metric_management);
+    }
+}

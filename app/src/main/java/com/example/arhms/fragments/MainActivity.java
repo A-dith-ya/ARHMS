33cfb4b56a960a39b4740805c82b013a06ac8fe2
bburciaga <burciagab16@gmail.com>
2023-12-07 15:53:17 -0800

    moved contents of dashboard to activity main. Turns out there was two activities on top of each other

--- app/src/main/java/com/example/arhms/fragments/MainActivity.java
+++ app/src/main/java/com/example/arhms/fragments/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.arhms.fragments;
 
-import android.widget.EditText;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.arhms.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(MainActivity.this, DashboardActivity.class));
+        TextView textViewForNavigation = findViewById(R.id.userMetrics);
+        setTextViewClickListener(textViewForNavigation, BasicUserCustomizablePageActivity.class);
+
+        TextView textViewForNavigation2 = findViewById(R.id.settings);
+        setTextViewClickListener(textViewForNavigation2, MetricManagementActivity.class);
+
+        TextView textViewForNavigation3 = findViewById(R.id.heartRateMonitor);
+        setTextViewClickListener(textViewForNavigation3, HeartRateActivity.class);
+
+        TextView textViewForNavigation4 = findViewById(R.id.temperatureMonitor);
+        setTextViewClickListener(textViewForNavigation4, TemperatureActivity.class);
+
+        TextView textViewForNavigation5 = findViewById(R.id.notifications);
+        setTextViewClickListener(textViewForNavigation5, NotificationActivity.class);
+
+//        startActivity(new Intent(MainActivity.this, DashboardActivity.class));
+    }
+
+    private void setTextViewClickListener(TextView textView, final Class<?> targetActivityClass) {
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, targetActivityClass));
+            }
+        });
     }
 }

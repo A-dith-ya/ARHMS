a101b4095b2c4d4b9c75eae9020d025f1a12d57b
bburciaga <burciagab16@gmail.com>
2023-12-07 14:26:07 -0800

    fixed notification system

--- app/src/main/java/com/example/arhms/fragments/MainActivity.java
+++ app/src/main/java/com/example/arhms/fragments/MainActivity.java
@@ -6,43 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import com.example.arhms.R;
-import com.example.arhms.interfaces.ISubject;
-import com.example.arhms.utils.observer.UserData;
-import com.example.arhms.interfaces.IAlertCommand;
-import com.example.arhms.utils.command.AlertInvoker;
-import com.example.arhms.utils.command.AlertSystem;
-import com.example.arhms.utils.command.HeartRateHigh;
-import com.example.arhms.utils.command.TemperatureLow;
 
 public class MainActivity extends AppCompatActivity {
     public MainActivity() {
     }
 @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ISubject subject = new UserData();
-        subject.setMeasurements(35,new long[]{120, 60});
-        subject.setMeasurements(36,new long[]{125, 60});
-        subject.setMeasurements(34,new long[]{130, 60});
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText editText = findViewById(R.id.editTextExample);
-
-        String s = subject.getObserver(0).getString();
-
-        editText.setText(s);
         startActivity(new Intent(MainActivity.this, DashboardActivity.class));
-        AlertSystem alertSystem = new AlertSystem(this);
-        AlertInvoker alertInvoker = new AlertInvoker();
-
-        IAlertCommand heartRateHigh = new HeartRateHigh(alertSystem);
-        IAlertCommand temperatureLow = new TemperatureLow(alertSystem);
-
-        alertInvoker.setAlertCommand(heartRateHigh);
-        alertInvoker.executeAlert();
-
-        alertInvoker.setAlertCommand(temperatureLow);
-        alertInvoker.executeAlert();
     }
 }
--- app/src/main/java/com/example/arhms/fragments/NotificationActivity.java
+++ app/src/main/java/com/example/arhms/fragments/NotificationActivity.java
@@ -1,9 +1,13 @@
 package com.example.arhms.fragments;
 
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import com.example.arhms.R;
+import com.example.arhms.interfaces.IAlertCommand;
+import com.example.arhms.utils.command.*;
 
 public class NotificationActivity extends AppCompatActivity {
 
@@ -11,5 +15,70 @@ public class NotificationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+        // Access the button by its ID
+        Button hrHighBTN = findViewById(R.id.hrHigh);
+        Button hrLowBTN = findViewById(R.id.hrLow);
+        Button temperatureHighBTN = findViewById(R.id.temperatureHigh);
+        Button temperatureLowBTN = findViewById(R.id.temperatureLow);
+
+        AlertSystem alertSystem = new AlertSystem(this);
+        AlertInvoker alertInvoker = new AlertInvoker();
+
+        IAlertCommand heartRateHigh = new HeartRateHigh(alertSystem);
+        IAlertCommand temperatureHigh = new TemperatureHigh(alertSystem);
+        IAlertCommand heartRateLow = new HeartRateLow(alertSystem);
+        IAlertCommand temperatureLow = new TemperatureLow(alertSystem);
+
+
+        // Set a click listener for the button
+        hrHighBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle button click event
+                alertInvoker.setAlertCommand(heartRateHigh);
+                heartRateHigh.execute();
+            }
+        });
+
+        // Set a click listener for the button
+        hrHighBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle button click event
+                alertInvoker.setAlertCommand(heartRateLow);
+                alertInvoker.executeAlert();
+            }
+        });
+
+        // Set a click listener for the button
+        hrLowBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle button click event
+                alertInvoker.setAlertCommand(heartRateLow);
+                alertInvoker.executeAlert();
+            }
+        });
+
+        // Set a click listener for the button
+        temperatureHighBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle button click event
+                alertInvoker.setAlertCommand(temperatureHigh);
+                alertInvoker.executeAlert();
+            }
+        });
+
+        // Set a click listener for the button
+        temperatureLowBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle button click event
+                alertInvoker.setAlertCommand(temperatureLow);
+                alertInvoker.executeAlert();
+            }
+        });
     }
 }

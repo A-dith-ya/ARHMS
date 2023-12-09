f3382ede6584fc493414b40abcf073e5710f3cd5
bburciaga <burciagab16@gmail.com>
2023-12-08 18:14:04 -0800

    automated heart rate

--- app/src/main/java/com/example/arhms/fragments/HeartRateActivity.java
+++ app/src/main/java/com/example/arhms/fragments/HeartRateActivity.java
@@ -1,15 +1,32 @@
 package com.example.arhms.fragments;
 
+import android.os.Handler;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.example.arhms.DataStorage;
 import com.example.arhms.R;
 
 public class HeartRateActivity extends AppCompatActivity {
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DataStorage instance = DataStorage.getInstance();
         setContentView(R.layout.activity_heart_rate);
+
+        handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                TextView tempMetricTextView = findViewById(R.id.heartRateText);
+
+                tempMetricTextView.setText("Heart Rate: " + Integer.toString(instance.generateRandomNumber(100, 160)) + " BPM");
+                handler.postDelayed(this, 1000);
+            }
+        }, 1000);
     }
 }
